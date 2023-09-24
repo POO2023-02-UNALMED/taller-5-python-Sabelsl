75ab80f7c83c2a5688acdefe912c50a095ca4440
Sabelsl <141966602+Sabelsl@users.noreply.github.com>
2023-09-24 15:38:05 -0500

    9

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -47,7 +47,11 @@ class Animal:
     
     @classmethod
     def totalPorTipo(cls):
-        
+        from zooAnimales.anfibio import Anfibio
+        from zooAnimales.ave import Ave
+        from zooAnimales.mamifero import Mamifero
+        from zooAnimales.pez import Pez
+        from zooAnimales.reptil import Reptil
         mensaje = "Mamiferos: " + str(Mamifero.cantidadMamiferos()) + "\nAves: " + str(Ave.cantidadAves()) + "\nReptiles: " + str(Reptil.cantidadReptiles()) + "\nPeces: " + str(Pez.cantidadPeces()) + "\nAnfibios: " + str(Anfibio.cantidadAnfibios())
         
         return mensaje
